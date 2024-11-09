7f18b219f9332fe53c5f4f8d767e193ed32137a7
kamal <anishkamal200@gmail.com>
2024-11-09 22:08:21 +0530

    optimized the code logic

--- com/java/Question1431.java
+++ com/java/Question1431.java
@@ -7,17 +7,18 @@ https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/descripti
 public class Question1431 {
     public static void main(String[] args) {
         System.out.println(kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3));
-        System.out.println(kidsWithCandies(new int[]{4,2,1,1,2}, 1));
-        System.out.println(kidsWithCandies(new int[]{12,1,12}, 10));
+        System.out.println(kidsWithCandies(new int[]{4, 2, 1, 1, 2}, 1));
+        System.out.println(kidsWithCandies(new int[]{12, 1, 12}, 10));
     }
 
     public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
 
         List<Boolean> result = new ArrayList<>();
 
+        int max = max(candies);
         for (int candy : candies) {
 
-            if (candy + extraCandies >= max(candies)) {
+            if (candy + extraCandies >= max) {
                 result.add(Boolean.TRUE);
             } else {
                 result.add(Boolean.FALSE);
