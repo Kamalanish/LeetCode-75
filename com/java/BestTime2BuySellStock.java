dd2f0eeee39969dc6c382a48b1539eb49520ed2b
kamal <anishkamal200@gmail.com>
2024-12-22 21:48:46 +0530

    updated

---
+++ com/java/BestTime2BuySellStock.java
@@ -0,0 +1,19 @@
+public class BestTime2BuySellStock {
+    // prices = [7,1,5,3,6,4]
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        int minPrice = Integer.MAX_VALUE; // represents  infinity
+
+        for (int price : prices){
+            if (price < minPrice){
+                minPrice = price;
+            }else {
+                int profit = price - minPrice;
+                maxProfit = Math.max(profit, maxProfit);
+            }
+        }
+        return maxProfit;
+
+    }
+}
